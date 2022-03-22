f55b3a212460542575f519674270b4b3feae40c4
Kenny <mccormick84@gmail.com>
2022-03-22 17:08:32 +0900

    Future Builder 실습 완료

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,10 +12,68 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final textStyle = TextStyle(fontSize: 16.0);
+
     return Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+            future: getNumber(),
+            builder: (context, snapshot) {
+
+              // 데이터가 있을 때 위젯 랜더링
+              if (!snapshot.hasData) {
+              }
+
+              // 에러가 났을 때 위젯 렌더링
+              if (!snapshot.hasData) {
+              }
+
+              // 로딩 중일 때 위젯 렌더링
+
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'FutureBuilder',
+                    style: textStyle.copyWith(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    'ConState: ${snapshot.connectionState}',
+                    style: textStyle,
+                  ),
+                  Text(
+                    'Data: ${snapshot.data}',
+                    style: textStyle,
+                  ),
+                  Text(
+                    'Data: ${snapshot.error}',
+                    style: textStyle,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {});
+                    },
+                    child: Text('setState'),
+                  ),
+                ],
+              );
+            }),
       ),
     );
+  }
+
+  Future<int> getNumber() async {
+    await Future.delayed(Duration(seconds: 3));
+
+    final random = Random();
+
+    // throw Exception('에러가 발생했습니다.');
+
+    return random.nextInt(100);
   }
 }
